39c774facbcb88df56d176bffbe87e5245d78ec0
chansreeniv <chansreeniv@yahoo.com>
2021-06-01 18:53:09 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,77 @@
+let model;
+let targetLabel;
+let trainingData = [];
+
+let state = 'collection';
+
+function setup(){
+    createCanvas(400,400);
+
+    let options = {
+        inputs: ['x', 'y'],
+        outputs:['label'],
+        task: 'classification',
+        debug: 'true'
+    }
+    model = ml5.neuralNetwork(options);
+}
+
+function keyPressed(){
+    if (key == 't') {
+        state = 'training';
+        console.log('training process to start')
+        model.normalizeData();
+        let options = {
+            epochs: 100
+        }
+        model.train(options, whileTraining, finishedTraining);
+    } else {
+        targetLabel = key.toUpperCase();
+    }
+}
+
+function whileTraining(epoch, loss){
+    console.log(epoch)
+}
+
+function finishedTraining(){
+    state = 'prediction';
+    console.log('finishedTraining');
+}
+
+function mousePressed(){
+
+    let inputs = {
+        x: mouseX,
+        y: mouseY
+    }
+
+    if(state == 'collection'){
+        let target ={
+            label: targetLabel
+        }
+        model.addData(inputs, target);
+        stroke(0);
+        noFill();
+        ellipse(mouseX, mouseY, 24);
+        fill(0);
+        noStroke();
+        textAlign(CENTER, CENTER);
+        text(targetLabel, mouseX, mouseY)
+    } else if(state == 'prediction'){
+        model.classify(inputs, gotResults);
+    }
+}
+
+function gotResults(err, results){
+    if(err){console.log(err); return}
+    console.log(results);
+        stroke(0);
+        fill(0,0,255,100);
+        ellipse(mouseX, mouseY, 24);
+        fill(0);
+        noStroke();
+        textAlign(CENTER, CENTER);
+        text(results[0].label, mouseX, mouseY)
+
+}
